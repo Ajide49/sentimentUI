d6414711d6ad167a53769ae8bbf1bf7866c9e97f
Isaac Wilkinson <isaacwilkinson@ladmins-MacBook-Pro-2.local>
2019-07-02 23:03:40 -0700

    added buttons for fees on sale web other

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './layout/layout.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { TwitterComponent } from './twitter/twitter.component';
 import { RedditComponent } from './reddit/reddit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent,
-    DashboardComponent,
     TwitterComponent,
     RedditComponent
   ],
